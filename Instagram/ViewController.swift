a56be166efa848db72f8be62297bc6faa6e17e0c
Aiyetin Roy <aiyetinroy@gmail.com>
2022-07-01 13:14:23 +0100

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Roy Aiyetin on 01/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
