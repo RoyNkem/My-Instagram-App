8388016e376df64eb9718fb8d9e3db9e2ccce220
Roy <95645767+RoyNkem@users.noreply.github.com>
2022-07-23 14:56:29 +0100

    Update contents to render in Feeds Header Cell like user info and Feeds action cell like like, comment and share buttons

--- Instagram/View/Cells/Home Feed & Post/InstagramFeedActionsTableViewCell.swift
+++ Instagram/View/Cells/Home Feed & Post/InstagramFeedActionsTableViewCell.swift
@@ -7,25 +7,91 @@
 
 import UIKit
 
+protocol InstagramFeedActionsTableViewCellDelegate: AnyObject {
+    func didTapLikeButton()
+    func didTapCommentButton()
+    func didTapShareButton()
+}
+
 final class InstagramFeedActionsTableViewCell: UITableViewCell {
+    
+    public weak var delegate: InstagramFeedActionsTableViewCellDelegate?
     
     static let identifier = "InstagramFeedActionsTableViewCell"
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.backgroundColor = .systemGreen
-    }
+    //MARK: - Declare UI Elememts
+    private let likeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "heart"), for: .normal)
+        button.tintColor = .label
+        return button
+    }()
     
+    private let commentButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = .label
+        button.setImage(UIImage(named: "message"), for: .normal)
+        return button
+    }()
+    
+    private let shareButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "arrow.uturn.right"), for: .normal)
+        button.tintColor = .label
+        return button
+    }()
+    
+    //MARK: INIT
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public func configure() {
-        //configure cell content
-
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubviews(likeButton, shareButton, commentButton)
+        
+        likeButton.addTarget(self, action: #selector(didTapLikeButton), for: .touchUpInside)
+        commentButton.addTarget(self, action: #selector(didTapCommentButton), for: .touchUpInside)
+        shareButton.addTarget(self, action: #selector(didTapShareButton), for: .touchUpInside)
     }
-
+    
+    @objc private func didTapLikeButton() {
+        delegate?.didTapLikeButton()
+    }
+    @objc private func didTapCommentButton() {
+        delegate?.didTapCommentButton()
+        
+    }
+    @objc private func didTapShareButton() {
+        delegate?.didTapShareButton()
+        
+    }
+    
+    public func configure(with post: String) {
+        //configure cell content
+        
+    }
+    
+    //MARK: Frame Layout
     override func layoutSubviews() {
         super.layoutSubviews()
+        
+        //like, comment, send buttons
+        //since the buttons are all similar, lets iterate through a single frame size using one variable: buttonSize
+        let buttonSize: CGFloat = contentView.height - 10
+        let buttons = [likeButton, commentButton, shareButton]
+        
+        for x in 0..<buttons.count {//Very smart
+            let button = buttons[x] // x = 0,1,2
+            button.frame = CGRect(x: (CGFloat(x) * buttonSize) + (10 * CGFloat(x+1)),
+                                  y: 5, width: buttonSize, height: buttonSize)
+        }
+        
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
     }
 }
--- Instagram/View/Cells/Home Feed & Post/InstagramFeedHeaderTableViewCell.swift
+++ Instagram/View/Cells/Home Feed & Post/InstagramFeedHeaderTableViewCell.swift
@@ -6,26 +6,84 @@
 //
 
 import UIKit
+import SDWebImage
+
+protocol InstagramFeedHeaderTableViewCellDelegate: AnyObject {
+    func didTapMoreButton()
+}
 
 final class InstagramFeedHeaderTableViewCell: UITableViewCell {
+    
+    public weak var delegate: InstagramFeedHeaderTableViewCellDelegate?
 
     static let identifier = "InstagramFeedHeaderTableViewCell"
     
+        //MARK: - Declare UI Elements
+    private let profilePhotoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        return label
+    }()
+    
+    private let moreButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = .label
+        button.setImage(UIImage(named: "ellipsis"), for: .normal)
+        return button
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.backgroundColor = .systemBlue
+        
+        contentView.addSubviews(profilePhotoImageView, usernameLabel, moreButton)
+        
+        moreButton.addTarget(self, action: #selector(didTapMoreButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapMoreButton() {
+        delegate?.didTapMoreButton()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public func configure() {
-        //configure cell content
+    //MARK: - Update Content of Header
+    public func configure(with model: User) {
+        usernameLabel.text = model.username
+        profilePhotoImageView.image = UIImage(named: "Roy2")
+        
+//        profilePhotoImageView.sd_setImage(with: model.profilePhoto, completed: nil)
 
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
+        let size = contentView.height/4
+        profilePhotoImageView.frame = CGRect(x: 2, y: 2, width: size, height: size)
+        profilePhotoImageView.layer.cornerRadius = size/2
+        
+        usernameLabel.frame = CGRect(x: profilePhotoImageView.right + 10, y: 2, width: contentView.width - (size - 2) - 15, height: contentView.height - 4)
+        
+        moreButton.frame = CGRect(x: contentView.width - size, y: 2, width: size, height: size)
+      
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        usernameLabel.text = nil
+        profilePhotoImageView.image = nil
     }
 }
